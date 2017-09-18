aafc4a9128251885f1b1be6e0539781dc1284bd2
zabir-nabil <zabiralnazi@codeassign.com>
2017-09-18 15:34:09 +0600

    DP

---
+++ Contest Upsolve/DP Level 2/LightOJ 1025/main.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+
+
+#define loop(i,s,e) for(int i = s;i<=e;i++) //including end point
+
+#define pb(a) push_back(a)
+
+#define sqr(x) ((x)*(x))
+
+#define CIN ios_base::sync_with_stdio(0); cin.tie(0);
+
+#define ll long long
+
+#define ull unsigned long long
+
+#define SZ(a) int(a.size())
+
+#define read() freopen("input.txt", "r", stdin)
+
+#define write() freopen("output.txt", "w", stdout)
+
+
+#define ms(a,b) memset(a, b, sizeof(a))
+
+#define all(v) v.begin(), v.end()
+
+#define PI acos(-1.0)
+
+#define pf printf
+
+#define sfi(a) scanf("%d",&a);
+
+#define sfii(a,b) scanf("%d %d",&a,&b);
+
+#define sfl(a) scanf("%lld",&a);
+
+#define sfll(a,b) scanf("%lld %lld",&a,&b);
+
+#define sful(a) scanf("%llu",&a);
+
+#define sfulul(a,b) scanf("%llu %llu",&a,&b);
+
+#define sful2(a,b) scanf("%llu %llu",&a,&b); // A little different
+
+#define sfc(a) scanf("%c",&a);
+
+#define sfs(a) scanf("%s",a);
+
+#define getl(s) getline(cin,s);
+
+#define mp make_pair
+
+#define paii pair<int, int>
+
+#define padd pair<dd, dd>
+
+#define pall pair<ll, ll>
+
+#define vi vector<int>
+
+#define vll vector<ll>
+
+#define mii map<int,int>
+
+#define mlli map<ll,int>
+
+#define mib map<int,bool>
+
+#define fs first
+
+#define sc second
+
+#define CASE(t) printf("Case %d: ",++t) // t initialized 0
+
+#define cCASE(t) cout<<"Case "<<++t<<": ";
+
+#define D(v,status) cout<<status<<" "<<v<<endl;
+
+#define INF 1000000000   //10e9
+
+#define EPS 1e-9
+
+#define flc fflush(stdout); //For interactive programs , flush while using pf (that's why __c )
+
+#define CONTEST 1
+using namespace std;
+
+
+//Bit Manipulation
+bool Check_ON(int mask,int pos) //Check if pos th bit (from right) of mask is ON
+{
+    if( (mask & (1<<pos) ) == 0  )return false;
+    return true;
+}
+int SET(int mask,int pos) //Save the returned mask into some var //Turn on pos th bit in mask
+{
+    return (mask | (1<<pos));
+}
+int RESET(int mask,int pos)  //Save the returned mask into some var //Turn off pos th bit in mask
+{
+    return (mask & ~(1<<pos));
+}
+int FLIP(int mask,int pos) //Save the returned mask into some var //Toggle/Flip pos th bit in mask
+{
+    return (mask ^ (1<<pos));
+}
+int LSB(int mask) // The actual LSB mask
+{
+    return (mask & (-mask));
+}
+int LSB_pos(int mask) // 0 based position
+{
+    int mask_2 = (mask & (-mask));
+    for(int pos = 0; pos<=15; pos++)
+    {
+        if(Check_ON(mask_2,pos))
+            return pos;
+    }
+    return -1;//
+}
+int ON_Bits(int mask)
+{
+    return __builtin_popcount(mask);
+}
+inline int clz(int N)   // O(1) way to calculate log2(X) (int s only)
+{
+    return N ? 32 - __builtin_clz(N) : -INF;
+}
+char curs[65];
+int n;
+
+/*
+
+For these types of combinatorial problems
+always use long long type.
+
+Ans usually becomes too big.
+
+I got some weird TLE s for using ints
+got AC in 0.004 seconds after making
+everything ll.
+
+*/
+ll dp[65][65];
+ll rec(int i,int j)
+{
+    if(i==j)
+    {
+        return 1;
+    }
+    if(i>j)
+        return 0;
+
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+
+    ll p1 = 0, p2 = 0, p3 = 0;
+
+    p1 = rec(i+1,j);
+    p2 = rec(i,j-1);
+
+
+    ll p = 0;
+    if(curs[i-1]==curs[j-1])
+    {
+        p = p1 + p2 + 1;
+    }
+    else
+    {
+        p3 = rec(i+1,j-1);
+        p = p1 + p2 - p3;
+    }
+
+    return dp[i][j] = p;
+
+
+
+}
+int main()
+{
+    int tc,cas=0;
+    sfi(tc);
+
+    getchar();
+
+    while(tc--)
+    {
+        gets(curs);
+        n = strlen(curs);
+        ms(dp,-1);
+        ll ans = rec(1,n);
+
+        CASE(cas);
+        pf("%lld\n",ans);
+
+
+    }
+    return 0;
+}
