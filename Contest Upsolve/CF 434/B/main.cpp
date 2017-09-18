87f148096e8fe414a715e0061c2613ba1c09ae82
zabir-nabil <zabiralnazi@codeassign.com>
2017-09-18 10:04:10 +0600

    cf

---
+++ Contest Upsolve/CF 434/B/main.cpp
@@ -0,0 +1,185 @@
+#include <bits/stdc++.h>
+
+
+#define loop(i,s,e) for(int i = s;i<=e;i++) //including end point
+
+#define pb(a) push_back(a)
+
+#define sqr(x) ((x)*(x))
+
+#define CIN ios_base::sync_with_stdio(0); cin.tie(0);
+
+#define ll long long
+
+#define ull unsigned long long
+
+#define SZ(a) int(a.size())
+
+#define read() freopen("input.txt", "r", stdin)
+
+#define write() freopen("output.txt", "w", stdout)
+
+
+#define ms(a,b) memset(a, b, sizeof(a))
+
+#define all(v) v.begin(), v.end()
+
+#define PI acos(-1.0)
+
+#define pf printf
+
+#define sfi(a) scanf("%d",&a);
+
+#define sfii(a,b) scanf("%d %d",&a,&b);
+
+#define sfl(a) scanf("%lld",&a);
+
+#define sfll(a,b) scanf("%lld %lld",&a,&b);
+
+#define sful(a) scanf("%llu",&a);
+
+#define sfulul(a,b) scanf("%llu %llu",&a,&b);
+
+#define sful2(a,b) scanf("%llu %llu",&a,&b); // A little different
+
+#define sfc(a) scanf("%c",&a);
+
+#define sfs(a) scanf("%s",a);
+
+#define getl(s) getline(cin,s);
+
+#define mp make_pair
+
+#define paii pair<int, int>
+
+#define padd pair<dd, dd>
+
+#define pall pair<ll, ll>
+
+#define vi vector<int>
+
+#define vll vector<ll>
+
+#define mii map<int,int>
+
+#define mlli map<ll,int>
+
+#define mib map<int,bool>
+
+#define fs first
+
+#define sc second
+
+#define CASE(t) printf("Case %d: ",++t) // t initialized 0
+
+#define cCASE(t) cout<<"Case "<<++t<<": ";
+
+#define D(v,status) cout<<status<<" "<<v<<endl;
+
+#define INF 1000000000   //10e9
+
+#define EPS 1e-9
+
+#define flc fflush(stdout); //For interactive programs , flush while using pf (that's why __c )
+
+#define CONTEST 1
+using namespace std;
+
+
+//Bit Manipulation
+bool Check_ON(int mask,int pos) //Check if pos th bit (from right) of mask is ON
+{
+    if( (mask & (1<<pos) ) == 0  )return false;
+    return true;
+}
+int SET(int mask,int pos) //Save the returned mask into some var //Turn on pos th bit in mask
+{
+    return (mask | (1<<pos));
+}
+int RESET(int mask,int pos)  //Save the returned mask into some var //Turn off pos th bit in mask
+{
+    return (mask & ~(1<<pos));
+}
+int FLIP(int mask,int pos) //Save the returned mask into some var //Toggle/Flip pos th bit in mask
+{
+    return (mask ^ (1<<pos));
+}
+int LSB(int mask) // The actual LSB mask
+{
+    return (mask & (-mask));
+}
+int LSB_pos(int mask) // 0 based position
+{
+    int mask_2 = (mask & (-mask));
+    for(int pos = 0; pos<=15; pos++)
+    {
+        if(Check_ON(mask_2,pos))
+            return pos;
+    }
+    return -1;//
+}
+int ON_Bits(int mask)
+{
+    return __builtin_popcount(mask);
+}
+inline int clz(int N)   // O(1) way to calculate log2(X) (int s only)
+{
+    return N ? 32 - __builtin_clz(N) : -INF;
+}
+set<int>una;
+int flta[105];
+int flra[105];
+int main()
+{
+    int n,m;
+    while(cin>>n>>m)
+    {
+        for(int i=1;i<=m;i++)
+        {
+            cin>>flra[i]>>flta[i];
+
+            if(flra[i]==n)
+            {
+                cout<<flta[i]<<endl;
+                return 0;
+            }
+        }
+
+        int ans = 1;
+
+        for(int nf=1;nf<=200;nf++)
+        {
+            for(int aft=1;aft<=200;aft++)
+            {
+                int flg = 1;
+                for(int j=1;j<=m;j++)
+                {
+                    if(flra[j]/nf + (flra[j]%nf!=0) != flta[j])
+                    {
+                        flg = 0;
+                    }
+                }
+
+                if(n/nf + (n%nf!=0)!=aft)
+                {
+                    flg = 0;
+                }
+
+                if(flg)
+                una.insert(aft);
+
+            }
+        }
+
+        if(una.size()==1)
+            cout<<*(una.begin())<<endl;
+        else
+            cout<<-1<<endl;
+
+        una.clear();
+
+
+    }
+
+    return 0;
+}
---
+++ Contest Upsolve/CF 434/C/main.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+
+
+#define loop(i,s,e) for(int i = s;i<=e;i++) //including end point
+
+#define pb(a) push_back(a)
+
+#define sqr(x) ((x)*(x))
+
+#define CIN ios_base::sync_with_stdio(0); cin.tie(0);
+
+#define ll long long
+
+#define ull unsigned long long
+
+#define SZ(a) int(a.size())
+
+#define read() freopen("input.txt", "r", stdin)
+
+#define write() freopen("output.txt", "w", stdout)
+
+
+#define ms(a,b) memset(a, b, sizeof(a))
+
+#define all(v) v.begin(), v.end()
+
+#define PI acos(-1.0)
+
+#define pf printf
+
+#define sfi(a) scanf("%d",&a);
+
+#define sfii(a,b) scanf("%d %d",&a,&b);
+
+#define sfl(a) scanf("%lld",&a);
+
+#define sfll(a,b) scanf("%lld %lld",&a,&b);
+
+#define sful(a) scanf("%llu",&a);
+
+#define sfulul(a,b) scanf("%llu %llu",&a,&b);
+
+#define sful2(a,b) scanf("%llu %llu",&a,&b); // A little different
+
+#define sfc(a) scanf("%c",&a);
+
+#define sfs(a) scanf("%s",a);
+
+#define getl(s) getline(cin,s);
+
+#define mp make_pair
+
+#define paii pair<int, int>
+
+#define padd pair<dd, dd>
+
+#define pall pair<ll, ll>
+
+#define vi vector<int>
+
+#define vll vector<ll>
+
+#define mii map<int,int>
+
+#define mlli map<ll,int>
+
+#define mib map<int,bool>
+
+#define fs first
+
+#define sc second
+
+#define CASE(t) printf("Case %d: ",++t) // t initialized 0
+
+#define cCASE(t) cout<<"Case "<<++t<<": ";
+
+#define D(v,status) cout<<status<<" "<<v<<endl;
+
+#define INF 1000000000   //10e9
+
+#define EPS 1e-9
+
+#define flc fflush(stdout); //For interactive programs , flush while using pf (that's why __c )
+
+#define CONTEST 1
+using namespace std;
+
+
+//Bit Manipulation
+bool Check_ON(int mask,int pos) //Check if pos th bit (from right) of mask is ON
+{
+    if( (mask & (1<<pos) ) == 0  )return false;
+    return true;
+}
+int SET(int mask,int pos) //Save the returned mask into some var //Turn on pos th bit in mask
+{
+    return (mask | (1<<pos));
+}
+int RESET(int mask,int pos)  //Save the returned mask into some var //Turn off pos th bit in mask
+{
+    return (mask & ~(1<<pos));
+}
+int FLIP(int mask,int pos) //Save the returned mask into some var //Toggle/Flip pos th bit in mask
+{
+    return (mask ^ (1<<pos));
+}
+int LSB(int mask) // The actual LSB mask
+{
+    return (mask & (-mask));
+}
+int LSB_pos(int mask) // 0 based position
+{
+    int mask_2 = (mask & (-mask));
+    for(int pos = 0; pos<=15; pos++)
+    {
+        if(Check_ON(mask_2,pos))
+            return pos;
+    }
+    return -1;//
+}
+int ON_Bits(int mask)
+{
+    return __builtin_popcount(mask);
+}
+inline int clz(int N)   // O(1) way to calculate log2(X) (int s only)
+{
+    return N ? 32 - __builtin_clz(N) : -INF;
+}
+int spc[3005];
+
+int isc(char c)
+{
+    if(c==' ')
+        return 0;
+    if(c=='a'
+            || c=='e'
+            ||c=='i'
+            ||c=='o'
+            ||c=='u')
+        return 0;
+
+    return 1;
+
+}
+int main()
+{
+    string ss;
+    //string bal = "iloveyou";
+    //bal.insert(1," ");
+//    cout<<bal<<endl;
+    while(cin>>ss)
+
+    {
+        string bb = ss;
+        int n = ss.length();
+        for(int i=2; i<ss.length(); i++)
+        {
+            if(ss[i]==' ')
+                continue;
+            if(isc(ss[i]) && isc(ss[i-1])
+                    && isc(ss[i-2])
+                    && (ss[i]!=ss[i-1]
+                        || ss[i]!=ss[i-2]
+                        || ss[i-2]!=ss[i-1])
+
+              )
+            {
+                //spc[i] = 1;
+
+                ss.insert(i," ");
+                i++;
+            }
+        }
+
+        for(int i=0; i<ss.length(); i++)
+        {
+
+            cout<<ss[i];
+        }
+        cout<<endl;
+
+        ms(spc,0);
+
+
+
+    }
+
+
+    return 0;
+}
+
