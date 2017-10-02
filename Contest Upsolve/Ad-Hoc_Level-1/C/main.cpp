b05b247afd02bf1e64c5d14e444210c86f2f8654
zabir-nabil <zabiralnazi@codeassign.com>
2017-10-03 00:06:05 +0600

    editorial

---
+++ Contest Upsolve/Ad-Hoc_Level-1/C/main.cpp
@@ -0,0 +1,188 @@
+#include <bits/stdc++.h>
+
+
+#define loop(i,s,e) for(int i = s;i<=e;i++) //including end point
+
+#define pb(a) push_back(a)
+
+#define sqr(x) ((x)*(x))
+
+#define CIN ios_base::sync_with_stdio(0); cin.tie(0);
+
+#define ll long long
+
+#define ull unsigned long long
+
+#define SZ(a) int(a.size())
+
+#define read() freopen("input.txt", "r", stdin)
+
+#define write() freopen("output.txt", "w", stdout)
+
+
+#define ms(a,b) memset(a, b, sizeof(a))
+
+#define all(v) v.begin(), v.end()
+
+#define PI acos(-1.0)
+
+#define pf printf
+
+#define sfi(a) scanf("%d",&a);
+
+#define sfii(a,b) scanf("%d %d",&a,&b);
+
+#define sfl(a) scanf("%lld",&a);
+
+#define sfll(a,b) scanf("%lld %lld",&a,&b);
+
+#define sful(a) scanf("%llu",&a);
+
+#define sfulul(a,b) scanf("%llu %llu",&a,&b);
+
+#define sful2(a,b) scanf("%llu %llu",&a,&b); // A little different
+
+#define sfc(a) scanf("%c",&a);
+
+#define sfs(a) scanf("%s",a);
+
+#define getl(s) getline(cin,s);
+
+#define mp make_pair
+
+#define paii pair<int, int>
+
+#define padd pair<dd, dd>
+
+#define pall pair<ll, ll>
+
+#define vi vector<int>
+
+#define vll vector<ll>
+
+#define mii map<int,int>
+
+#define mlli map<ll,int>
+
+#define mib map<int,bool>
+
+#define fs first
+
+#define sc second
+
+#define CASE(t) printf("Case %d: ",++t) // t initialized 0
+
+#define cCASE(t) cout<<"Case "<<++t<<": ";
+
+#define D(v,status) cout<<status<<" "<<v<<endl;
+
+#define INF 1000000000   //10e9
+
+#define EPS 1e-9
+
+#define flc fflush(stdout); //For interactive programs , flush while using pf (that's why __c )
+
+#define CONTEST 1
+using namespace std;
+
+
+//Bit Manipulation
+bool Check_ON(int mask,int pos) //Check if pos th bit (from right) of mask is ON
+{
+    if( (mask & (1<<pos) ) == 0  )return false;
+    return true;
+}
+int SET(int mask,int pos) //Save the returned mask into some var //Turn on pos th bit in mask
+{
+    return (mask | (1<<pos));
+}
+int RESET(int mask,int pos)  //Save the returned mask into some var //Turn off pos th bit in mask
+{
+    return (mask & ~(1<<pos));
+}
+int FLIP(int mask,int pos) //Save the returned mask into some var //Toggle/Flip pos th bit in mask
+{
+    return (mask ^ (1<<pos));
+}
+int LSB(int mask) // The actual LSB mask
+{
+    return (mask & (-mask));
+}
+int LSB_pos(int mask) // 0 based position
+{
+    int mask_2 = (mask & (-mask));
+    for(int pos = 0; pos<=15; pos++)
+    {
+        if(Check_ON(mask_2,pos))
+            return pos;
+    }
+    return -1;//
+}
+int ON_Bits(int mask)
+{
+    return __builtin_popcount(mask);
+}
+inline int clz(int N)   // O(1) way to calculate log2(X) (int s only)
+{
+    return N ? 32 - __builtin_clz(N) : -INF;
+}
+//50^3 = 125000
+//so, each term can be at max 50
+//do some pruning
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        int ans = 0;
+
+        for(int a=0; a<=50; a++)
+        {
+
+
+            int a3 = a*a*a;
+            if(a3>n)
+                break;
+            for(int b=a; b<=50; b++)
+            {
+                int b3 = a3 + b*b*b;
+                if(b3>n)
+                    break;
+                for(int c=b; c<=50; c++)
+                {
+                    int c3 = b3 + c*c*c;
+                    if(c3>n)
+                        break;
+                    for(int d=c; d<=50; d++)
+                    {
+                        int d3 = c3 + d*d*d;
+                        if(d3>n)
+                            break;
+                        for(int e=d; e<=50; e++)
+                        {
+                            int e3 = d3 + e*e*e;
+                            if(e3>n)
+                                break;
+                            //cout<<e3<<" ";
+
+                            if(e3==n)
+                                ans++;
+
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+
+        cout<<ans<<endl;
+
+
+    }
+
+
+    return 0;
+}
---
+++ Contest Upsolve/Ad-Hoc_Level-1/README.md
@@ -0,0 +1,33 @@
+# Editorial
+
+---------------------------------------------------------------------------------------------
+
+## Problem K
+
+> Tags : Implementation, Brute Force
+
+> Difficulty : Easy
+
+**Solution idea :**
+
+ * Just place all pairs of tiles in all possible orientation
+ * Report the maximum area that comes from some pair
+ 
+*Lots of if else s, don't miss any*
+   
+   
+   
+---------------------------------------------------------------------------------------------
+
+## Problem C
+
+> Tags : Implementation, Brute Force, Mathematics
+
+> Difficulty : Easy
+
+**Solution idea :**
+
+ * As, n = 125000. so, the loop variables can't exceed 50.
+ * Do some pruning.
+ 
+*Don't count repetition*
